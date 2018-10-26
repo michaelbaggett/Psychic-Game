a7a2f214fdb09efc64e0fd020fd4d73dca31af63
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-26 17:29:41 -0500

    html skeleton added. javascript pseudo code barebone added

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+<javascript>
+//array of letters
+//computer generates random letter
+
+//wins
+//losses
+//letters guessed
+
+//onkeyup function
+
+//compare userguess to computer generated letter
+
+//win condition
+//loss condition
+
+
+
+
+
+</javascript>
